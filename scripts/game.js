01028f2cbc9bced24eaa8eb330656199ffd9977d
DATA FOB <dataletra@gmail.com>
2026-08-13 15:14:09 +0200

    Feat: classes added and refactored

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,10 @@
-console.log("test");
+let canvas;
+let ctx;
+let world = new World();
+function init() {
+	canvas = document.getElementById("canvas");
+	ctx = canvas.getContext("2d");
+	console.log(world.character);
+}
+
+init();
