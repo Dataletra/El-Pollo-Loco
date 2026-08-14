147faf1cbda28a10ece0e9f15167131ec5f7446f
DATA FOB <dataletra@gmail.com>
2026-08-14 14:46:43 +0200

    feat: animated chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,9 +2,25 @@ class Chicken extends MovableObject {
 	y = 360;
 	height = 70;
 	width = 80;
+	IMAGES_WALKING = [
+		"./assets/img/enemies/gallina-1.png",
+		"./assets/img/enemies/gallina-2.png",
+		"./assets/img/enemies/gallina-3.png",
+	];
+	currentImage = 0;
 	constructor() {
 		super();
 		super.loadImage("./assets/img/enemies/gallina-1.png");
 		this.x = 200 + Math.random() * 500;
+		this.loadImages(this.IMAGES_WALKING);
+		this.animate();
+	}
+	animate() {
+		setInterval(() => {
+			let i = this.currentImage % this.IMAGES_WALKING.length;
+			let path = this.IMAGES_WALKING[i];
+			this.img = this.imageCache[path];
+			this.currentImage++;
+		}, 100);
 	}
 }
